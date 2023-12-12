25e0db624345670c7be779a33233d75c3d19ee7b
fai-sas <craftedbyfaisal@gmail.com>
2023-12-12 18:14:05 +0600

    recommit due to previous file error

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt')
+
+const { createTokenUser } = require('./createTokenUser')
+const { checkPermissions } = require('./checkPersmissions')
+
+module.exports = {
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+  createTokenUser,
+  checkPermissions,
+}
